ddc967d9cf42f926b44f02258c74a4b8aac6f833
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-10-25 20:05:42 +0200

    style: application success page

--- src/app/applications/success/page.tsx
+++ src/app/applications/success/page.tsx
@@ -1,6 +1,6 @@
 const KKBSuccessPage = () => {
   return (
-    <div className="h-dvh w-dvw flex items-center justify-center">
+    <div className="h-dvh flex items-center justify-center">
       <p className="max-w-xl font-semibold text-center">
         You have successfully submitted your application for the KKB program.
         You will get back to you soon! Keep on eye on your email inbox.
