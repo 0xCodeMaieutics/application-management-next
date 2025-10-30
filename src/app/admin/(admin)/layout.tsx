d53168025859ef0854069f9f8cd2023f75b4a99b
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-10-30 09:14:48 +0100

    refactor: removed unused comments

--- src/app/admin/(admin)/layout.tsx
+++ src/app/admin/(admin)/layout.tsx
@@ -1,30 +1,7 @@
 import { PropsWithChildren } from "react";
 
 const AdminLayout = ({ children }: PropsWithChildren) => {
-  
-  return <div>Admin layout {children}</div>;
+  return <div>{children}</div>;
 };
 
 export default AdminLayout;
-
-
-// KKB 3 months -> Arbeitserlaubnis
-// KKB 8 months -> Visa
-
-/**
- * Aplicant
- * 1. Form submission
- * 2. Application RECEIVED
- * 3. Admin changes the status to IN_PROGRESS
- * 4. DONE
- */
-
-// RECEIVED -> IN_PROGRESS -> ACCEPTED or CANCELLED
-
-/**
- * Requested features:
- * 1. Edit applications profiles
- * 2. Office notice (Giuli or other offices)
- * 3. Search bar for applications (name, instagram, email, phone)
- * 4. Delete applications
- */
